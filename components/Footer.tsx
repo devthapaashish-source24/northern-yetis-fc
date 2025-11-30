e3970fd60e8b5b2e66378eda8d095589906537e5
Ashish Thapa <devthapaashish@gmail.com>
2025-11-29 19:41:50 -0500

    small changed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="px-6 py-4 text-sm text-center bg-dark text-secondary">
       &copy; {new Date().getFullYear()} Northern Yetis FC. All rights reserved.
       <br />
-  <span className="italic text-muted"><span className="font-light">Developed by: </span><a className="hover:text-indigo-400" href="https://www.facebook.com/ashish.thapa.1213/" target="_blank">Ashish Nasri Thapa</a> </span>
+  <span className="italic text-muted"><span className="font-light">Developed by: </span><a className="hover:text-indigo-400" href="https://www.facebook.com/ashish.thapa.1213/" target="_blank">Ashish Thapa</a> </span>
     </footer>
   );
 }
